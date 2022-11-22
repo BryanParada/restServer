dee78223e3e191be0375edad6c4b0d6273d6c1bb
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-22 13:56:37 -0300

    N143 - changing _id for _uid

--- models/user.js
+++ models/user.js
@@ -35,7 +35,8 @@ const UserSchema = Schema({
 });
 
 UserSchema.methods.toJSON = function() {
-    const { __v, password, ...user} = this.toObject();
+    const { __v, password, _id, ...user} = this.toObject();
+    user._uid = _id;
     return user;
 }
 
