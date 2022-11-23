e8e033a24bc439a70e0bd8da50ab195ef922b388
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-23 18:00:40 -0300

    N166 - Crud & Endpoints

--- models/server.js
+++ models/server.js
@@ -8,8 +8,13 @@ class Server{
         this.app = express();
         this.port = process.env.PORT;
 
-        this.usersRoutesPath = '/api/users';
-        this.authPath        = '/api/auth';
+        this.paths = {
+            auth: '/api/auth',
+            categories: '/api/categories',
+            users: '/api/users'
+        }
+        // this.usersRoutesPath = '/api/users';
+        // this.authPath        = '/api/auth'; 
 
         //Conectar a base de datos
         this.connectDB();
@@ -39,8 +44,9 @@ class Server{
 
     routes(){
         
-        this.app.use(this.authPath, require('../routes/auth.routes'))
-        this.app.use(this.usersRoutesPath, require('../routes/user.routes'))
+        this.app.use(this.paths.auth, require('../routes/auth.routes'))
+        this.app.use(this.paths.users, require('../routes/user.routes'))
+        this.app.use(this.paths.categories, require('../routes/categories.routes'))
 
 
     }
